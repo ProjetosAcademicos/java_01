a9ba6940b17d40034deeba1178dd7cc021cbda33
Josinaldo <josinaldorobertorocha@gmail.com>
2016-05-21 10:59:26 -0300

    Alterei - Josinaldo

--- src/java_01/testeFinanceiro.java
+++ src/java_01/testeFinanceiro.java
@@ -4,6 +4,7 @@ public class testeFinanceiro {
 
 	public static void main(String[] args) {
 		System.out.println("teste git.... FUNCIONOU - giovanni");
+		System.out.println("teste git.... FUNCIONOU - Josinaldo");
 	}
 
 }
