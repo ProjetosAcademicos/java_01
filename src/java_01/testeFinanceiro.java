207844b6f5f727f82b2d10ae4ecf484055782ce7
Giovanni Nicoletti <giovanni.nic@gmail.com>
2016-05-21 10:51:45 -0300

    ghg

--- src/java_01/testeFinanceiro.java
+++ src/java_01/testeFinanceiro.java
@@ -3,7 +3,7 @@ package java_01;
 public class testeFinanceiro {
 
 	public static void main(String[] args) {
-		System.out.println("teste git.... FUNCIONOU");
+		System.out.println("teste git.... FUNCIONOU - giovanni");
 	}
 
 }
