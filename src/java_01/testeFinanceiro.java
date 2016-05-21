2c2076ff4638265f877f39f2a12b74011618946d
GlaudsonMarcel <GlaudsonMarcel@10.12.22.113>
2016-05-21 11:33:28 -0300

    teste

--- src/java_01/testeFinanceiro.java
+++ src/java_01/testeFinanceiro.java
@@ -5,6 +5,7 @@ public class testeFinanceiro {
 	public static void main(String[] args) {
 		System.out.println("teste git.... FUNCIONOU - giovanni");
 		System.out.println("teste git.... FUNCIONOU - Josinaldo");
+		System.out.println("teste git.... FUNCIONOU - MArcel");
 	}
 
 }
