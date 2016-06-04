bb0b2ee72dfda976573a5a5574856841729995c6
Giovanni Nicoletti <giovanni.nic@gmail.com>
2016-06-04 17:08:15 -0300

    toString

--- src/br/com/ifinance/beans/PessoaJuridica.java
+++ src/br/com/ifinance/beans/PessoaJuridica.java
@@ -20,5 +20,19 @@ public class PessoaJuridica extends Pessoa {
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
+	
+	@Override
+	public String toString() {
+		StringBuilder strUtil = new StringBuilder();
+		strUtil.append("CNPJ: " + getCnpj()+"\n"); 
+		strUtil.append("Nome: " + getNome()+"\n");
+		strUtil.append("Endereco: " + getEndereco()+"\n");
+		strUtil.append("Bairro: " + getBairro()+"\n");
+		strUtil.append("Cidade: " + getCidade()+"\n");
+		strUtil.append("CEP: " + getCep()+"\n");
+		strUtil.append("login: " + getUsuario().getLogin()+"\n");		
+		
+		return strUtil.toString();
+	}
 
 }
